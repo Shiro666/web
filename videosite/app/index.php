1167d564587509abb9ff5871dd5e03c897796181
Shiro <932348537@qq.com>
2018-09-27 10:47:38 +0800

    修改了视频模块的头部

--- videosite/app/index.php
+++ videosite/app/index.php
@@ -12,43 +12,6 @@ $rank = udata($rank, $db);
 ?>
 
 
-
-            <!-- layerslider -->
-            <section id="slider">
-                <div id="full-slider-wrapper">
-                    <div id="layerslider" style="width:100%;height:500px;">
-
-                        <?php
-                        if(!empty($adsdata))
-                        {
-                        foreach ($adsdata as $i=>$d)
-                        {
-                        ?>
-
-
-
-                        <div class="ls-slide" data-ls="transition2d:1;timeshift:-1000;">
-                            <img src="../<?php echo $d['IMAGE_URL'];?>" class="ls-bg" alt="Slide background"/>
-                            <h3 class="ls-l" style="left:50px; top:135px; padding: 15px; color: #444444;font-size: 24px;font-family: 'Open Sans'; font-weight: bold; text-transform: uppercase;" data-ls="offsetxin:0;durationin:2500;delayin:500;durationout:750;easingin:easeOutElastic;rotatexin:90;transformoriginin:50% bottom 0;offsetxout:0;rotateout:-90;transformoriginout:left bottom 0;">
-                                <?php echo $d['TITLE'];?></h3>
-                            <h1 class="ls-l" style="left: 63px; top:185px;background: #e96969;padding:0 10px; opacity: 1; color: #ffffff; font-size: 36px; font-family: 'Open Sans'; text-transform: uppercase; font-weight: bold;" data-ls="offsetxin:left;durationin:3000; delayin:800;durationout:850;rotatexin:90;rotatexout:-90;">
-                                <?php echo $d['INTRO'];?>
-                            </h1>
-
-                            <a href="<?php echo $d['LINK_URL'];?>" target="_blank" class="ls-l button" style="border-radius:4px;text-align:center;left:63px; top:315px;background: #444;color: #ffffff;font-family: 'Open Sans';font-size: 14px;display: inline-block; text-transform: uppercase; font-weight: bold;" data-ls="durationout:850;offsetxin:90;offsetxout:-90;duration:4200;fadein:true;fadeout:true;">
-                                点击查看</a>
-
-                            <img class="ls-l ls-linkto-2" style="top:400px;left:50%;white-space: nowrap;" data-ls="offsetxin:-50;delayin:1000;rotatein:-40;offsetxout:-50;rotateout:-40;" src="images/sliderimages/left.png" alt="">
-                            <img class="ls-l ls-linkto-2" style="top:400px;left:52%;white-space: nowrap;" data-ls="offsetxin:50;delayin:1000;offsetxout:50;" src="images/sliderimages/right.png" alt="">
-                        </div>
-
-                        <?php }} ?>
-
-
-                    </div>
-                </div>
-            </section><!--end slider-->
-
             <!-- Premium Videos -->
             <section id="premium">
                 <div class="row">
