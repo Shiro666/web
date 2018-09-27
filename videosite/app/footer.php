98ad48cec5687d9a34bc6cdb6d977a559f704792
Shiro <932348537@qq.com>
2018-09-27 12:04:15 +0800

    修改视频模块脚部

--- videosite/app/footer.php
+++ videosite/app/footer.php
@@ -1,18 +1,15 @@
 
 
-                <!-- popular Videos -->
-
-            <div id="footer-bottom">
-                <div class="logo text-center">
-                    <img src="images/footerlogo.png" alt="footer logo">
-                </div>
-                <div class="btm-footer-text text-center">
-                    <p>2018 © 防诈骗中心.</p>
-                </div>
-            </div>
         </div><!--end off canvas content-->
     </div><!--end off canvas wrapper inner-->
 </div><!--end off canvas wrapper-->
+<div class="grid_16 b footer">
+    <div class="grid_16" id="botton">
+        地址：广州市番禺区广州大学城 邮编：510006
+        <br>
+        版权所有：诈骗信息发布网站 &copy;CopyRight:www.fraudinfopublishingwebsite.com
+    </div>
+</div>
 <!-- script files -->
 <script src="bower_components/jquery/dist/jquery.js"></script>
 <script src="bower_components/what-input/what-input.js"></script>
@@ -20,6 +17,10 @@
 <script src="js/jquery.showmore.src.js" type="text/javascript"></script>
 <script src="js/app.js"></script>
 <script src="layerslider/js/greensock.js" type="text/javascript"></script>
+<script src="https://cdn.bootcss.com/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://cdn.bootcss.com/jquery-cookie/1.4.1/jquery.cookie.min.js"></script>
+<script src="../../html/scripts/vision1.0.min.js"></script>
+<script src="../../html/scripts/scripts1525.min.js"></script>
 <!-- LayerSlider script files -->
 <script src="layerslider/js/layerslider.transitions.js" type="text/javascript"></script>
 <script src="layerslider/js/layerslider.kreaturamedia.jquery.js" type="text/javascript"></script>
--- videosite/app/header.php
+++ videosite/app/header.php
@@ -45,14 +45,57 @@ function udata($data,$db)
     <link rel="stylesheet" href="css/app.css">
     <link rel="stylesheet" href="css/theme.css">
     <link rel="stylesheet" href="css/font-awesome.min.css">
-
     <link rel="stylesheet" type="text/css" href="layerslider/css/layerslider.css">
     <link rel="stylesheet" href="css/owl.carousel.min.css">
     <link rel="stylesheet" href="css/owl.theme.default.min.css">
     <link rel="stylesheet" href="css/responsive.css">
+    <link rel="stylesheet" href="../../html/css/reset.min.css">
+    <link rel="stylesheet" href="../../html/css/text.min.css">
+    <link rel="stylesheet" href="../../html/css/960_16_col.min.css">
+    <link rel="stylesheet" href="../../html/css/color.min.css">
+    <link rel="stylesheet" href="../../html/css/layout.min.css">
+    <link rel="stylesheet" href="../../html/css/typography.min.css">
 </head>
 <body>
-<div class="off-canvas-wrapper">
-    <div class="off-canvas-wrapper-inner" data-off-canvas-wrapper>
-        <div class="off-canvas-content" data-off-canvas-content>
-            
+    <script type="text/javascript" src="../../html/scripts/WebP.js"></script>
+    
+    <div class="container_16 clearfix lo">
+        <div class="grid_16 login_register">
+            <span id="Aprf1"></span>
+            <span id="Aprf2"><a id="login_button" onclick="showDialog()" href="javascript:;">[登录]</a><a href="REGISTER.html">&nbsp;&nbsp;&nbsp;&nbsp;[注册]</a> </span>
+        </div>
+        <div id="header" class="grid_16">
+            <img src="../../html/images/banner.jpg">
+            <div id="nav">
+                <ul>
+                    <li class="nav_content"><a href="../../html/HOME.html">&nbsp;&nbsp;&nbsp;&nbsp;首页</a></li>
+                    <li class="mid">|</li>
+                    <li class="nav_content"><a href="../../html/ANLI_LIST.html?case_type=1">支付宝诈骗</a></li>
+                    <li class="mid">|</li>
+                    <li class="nav_content"><a href="../../html/ANLI_LIST.html?case_type=2">商业诈骗</a> </li>
+                    <li class="mid">|</li>
+                    <li class="nav_content"><a href="../../html/ANLI_LIST.html?case_type=3">手机银行诈骗</a></li>
+                    <li class="mid">|</li>
+                    <li class="nav_content"><a href="../../html/ANLI_LIST.html?case_type=4">网络诈骗</a></li>
+                    <li class="mid">|</li>
+                    <li class="nav_content"><a href="../../html/ANLI_LIST.html?case_type=5">网购诈骗</a></li>
+                    <li class="mid">|</li>
+                    <li class="nav_content"><a href="../../html/ANLI_LIST.html?case_type=6">社会诈骗</a></li>
+                    <li class="mid">|</li>
+                    <li class="nav_content"><a href="../../html/ANLI_LIST.html?case_type=7">电信诈骗</a></li>
+                    <li class="mid">|</li>
+                    <li class="nav_content"><a href="../../html/ANLI_LIST.html?case_type=8">微信诈骗</a></li>
+                    <li class="mid">|</li>
+                    <li class="nav_content"><a href="../../html/BBS.html?post_type=0">网页论坛</a></li>
+                    <li class="mid">|</li>
+                    <li class="nav_content"><a href="index.php">视频天地</a></li>
+                    <li class="mid">|</li>
+                    <li class="nav_content"><a href="../../html/SEARCH.html">检索</a></li>
+                    <li class="mid">|</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="off-canvas-wrapper">
+        <div class="off-canvas-wrapper-inner" data-off-canvas-wrapper>
+            <div class="off-canvas-content" data-off-canvas-content>
